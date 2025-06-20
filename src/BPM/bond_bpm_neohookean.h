f0a98f9778adea92381da82a21306e04485f8151
SoftMatterLab24 <softmattermechanics@gmail.com>
2025-06-20 10:58:24 -0600

    create templates for neohooean bond style

---
+++ src/BPM/bond_bpm_neohookean.h
@@ -0,0 +1,23 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef BOND_CLASS
+// clang-format off
+BondStyle(bpm/neohookean,BondBPMNeohookean);
+// clang-format on
+#else
+
+#ifndef LMP_BOND_BPM_PRONY_H
+#define LMP_BOND_BPM_PRONY_H
+
+#include "bond_bpm.h"
